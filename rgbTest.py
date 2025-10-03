0c1f1848645a9a474cc4b49cd5caa261373fc8b2
zzmz <2200421816@qq.com>
2025-10-04 03:27:32 +0800

    0.01:基本完成识别，但不能区分植物需求类型，定位准确率不高，但效率高于手动操作，添加切换工具功能，开始/暂停/中断功能。

--- rgbTest.py
+++ rgbTest.py
@@ -2,19 +2,21 @@ import cv2
 import numpy as np
 
 def find_contours(binary_src , min_area):
-    contours, hierarchy = cv2.findContours(binary_src, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(binary_src, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours is None or len(contours) == 0:
+        return None
+    filtered_contours = []
     for i , cnt in enumerate(contours):
-        if cnt is None:
-            print("cnt is None")
-            continue
+
         area = cv2.contourArea(cnt)  # 普通面积（正数）
         if area < min_area:
             continue
-    cv2.drawContours(binary_src, [cnt], -1, (0, 0, 255), 2)
-    return
-def preprocess(src):
+        filtered_contours.append(cnt)
+    return filtered_contours
+
+def preprocess(srcs):
     # 分离颜色通道
-    bgr_images = cv2.split(src)
+    bgr_images = cv2.split(srcs)
 
     # 获取各个通道
     r_src_img = bgr_images[2]  # 红色通道
@@ -27,7 +29,7 @@ def preprocess(src):
     a_src_img = cv2.subtract(a_src_img, g_src_img)
 
     # 对 a_src_img 进行模糊处理
-    blur_a_src = cv2.blur(a_src_img, (3, 3))
+    blur_a_src = cv2.blur(a_src_img, (4, 2))
 
     # 以下是被注释掉的代码
     # threshold(a_src_img, gray_src, 90, 255, THRESH_BINARY)
@@ -43,8 +45,6 @@ def preprocess(src):
     gray_src = cv2.morphologyEx(gray_src, cv2.MORPH_OPEN, dst2)
 
     # 显示图像
-    cv2.imshow("r_src_img", r_src_img)
-    cv2.imshow("gray_src", gray_src)
 
     return gray_src
 
@@ -58,6 +58,19 @@ if __name__ == "__main__":
         exit()
     # 调用预处理函数
     result = preprocess(src)
+    filtered_contour=find_contours(result, 100 )
+    print([filtered_contour])
+    if filtered_contour is None:
+        print("未找到符合条件的轮廓")
+        exit()
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    cv2.drawContours(src, filtered_contour, -1, (255, 0, 0), 2)
+    print([filtered_contour])
+
+    cv2.imshow("result", result)
+    cv2.imshow("src", src)
+
+
     # 等待按键并关闭窗口
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- screenReader.py
+++ screenReader.py
@@ -5,22 +5,63 @@ from PIL import Image
 import os
 from pynput import keyboard
 import time
-
-
 class KeyboardMonitor:
     def __init__(self):
+        self.status = 'pause'  # 初始状态为暂停
+        self.tool = '1'  # 初始工具为浇水
         self.listener = None
         self.is_running = False
 
 
     def on_release(self, key):
+        if key == keyboard.Key.f1:
+            # ESC键停止监听
+            self.tool = '1'
+            print('浇水模式')
+            return self.tool
+
+        if key == keyboard.Key.f2:
+            # ESC键停止监听
+            self.tool = '2'
+            print('喂食模式')
+            return self.tool
+
+        if key == keyboard.Key.f3:
+            # ESC键停止监听
+            self.tool = '3'
+            print('除虫模式')
+            return self.tool
+
+        if key == keyboard.Key.f4:
+            # ESC键停止监听
+            self.tool = '4'
+            print('音乐模式')
+            return self.tool
+
         if key == keyboard.Key.esc:
             # ESC键停止监听
-            return False
+            self.status = 'shutdown'
+            print('Shutdown')
+            return self.status
 
+        if key == keyboard.Key.caps_lock:
+            self.status = 'start'
+            print('Start')
+            return self.status
+
+        if key == keyboard.Key.shift:
+            self.status = 'pause'
+            print('Pause')
+            return self.status
     def start(self):
         """启动键盘监听（非阻塞）"""
         self.is_running = True
+        if self.on_release == 'shutdown':
+            self.stop()
+        if self.on_release == 'start':
+            self.is_running = True
+        if self.on_release == 'pause':
+            self.is_running = False
         self.listener = keyboard.Listener(
             on_release=self.on_release)
         self.listener.start()
@@ -37,32 +78,23 @@ class KeyboardMonitor:
         """检查监听器是否在运行"""
         return self.listener and self.listener.is_alive()
 
-def get_top_n_values(result, n=5):
-    flat_result = result.flatten()  # 将二维数组展平为一维
-    indices = np.argpartition(flat_result, -n)[-n:]  # 获取前 n 个最大值的索引
-    top_values = flat_result[indices]  # 获取对应的值
-    sorted_indices = indices[np.argsort(-top_values)]  # 按值降序排序索引
-    sorted_values = top_values[np.argsort(-top_values)]  # 按值降序排序值
 
-     # 将一维索引转换为二维坐标
-    positions = [np.unravel_index(idx, result.shape) for idx in sorted_indices]
-    return list(zip(sorted_values, positions))
-
-def preprocess(src, gray_src):
+def preprocess(srcs):
     # 分离颜色通道
-    bgr_images = cv2.split(src)
+    bgr_images = cv2.split(srcs)
 
     # 获取各个通道
     r_src_img = bgr_images[2]  # 红色通道
-    g_src_img = bgr_images[1]  # 绿色通道
+    g_src_img = bgr_images[1]  # 绿色通道（注意：原C++代码中这里索引为0）
     b_src_img = bgr_images[0]  # 蓝色通道
 
     # 计算 a_src_img = r_src_img + 10 - g_src_img
-    a_src_img = cv2.add(r_src_img, 10)
+    a_src_img = cv2.add(b_src_img, 10)
+    a_src_img = cv2.subtract(a_src_img, r_src_img)
     a_src_img = cv2.subtract(a_src_img, g_src_img)
 
     # 对 a_src_img 进行模糊处理
-    blur_a_src = cv2.blur(a_src_img, (3, 3))
+    blur_a_src = cv2.blur(a_src_img, (4, 2))
 
     # 以下是被注释掉的代码
     # threshold(a_src_img, gray_src, 90, 255, THRESH_BINARY)
@@ -71,24 +103,28 @@ def preprocess(src, gray_src):
     # morphologyEx(gray_src, gray_src, MORPH_CLOSE, dst2)
 
     # 对蓝色通道进行阈值处理
-    _, gray_src = cv2.threshold(b_src_img, 90, 255, cv2.THRESH_BINARY)
+    _, gray_src = cv2.threshold(b_src_img, 254, 255, cv2.THRESH_BINARY)
 
     # 创建结构元素并进行闭操作
-    dst2 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    gray_src = cv2.morphologyEx(gray_src, cv2.MORPH_CLOSE, dst2)
+    dst2 = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 2))
+    gray_src = cv2.morphologyEx(gray_src, cv2.MORPH_OPEN, dst2)
 
     # 显示图像
-    cv2.imshow("r_src_img", b_src_img)
-    cv2.imshow("gray_src", gray_src)
 
     return gray_src
 
 def find_contours(binary_src , min_area):
-    contours, hierarchy = cv2.findContours(binary_src, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(binary_src, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours is None or len(contours) == 0:
+        return None
+    filtered_contours = []
     for i , cnt in enumerate(contours):
+
         area = cv2.contourArea(cnt)  # 普通面积（正数）
         if area < min_area:
             continue
+        filtered_contours.append(cnt)
+    return filtered_contours
 
 class ScreenPatternAutomation:
     def __init__(self, confidence_threshold):
@@ -101,109 +137,55 @@ class ScreenPatternAutomation:
         self.confidence_threshold = confidence_threshold
         self.template_images = {}
 
-    def load_template(self, template_name, template_path):
-        """
-        加载模板图像
+    # def load_template(self, template_name, template_path):
+    #     """
+    #     加载模板图像
+    #
+    #     Args:
+    #         template_name (str): 模板名称
+    #         template_path (str): 模板图像文件路径
+    #     """
+    #     if not os.path.exists(template_path):
+    #         raise FileNotFoundError(f"模板文件不存在: {template_path}")
+    #
+    #     template = cv2.imread(template_path, cv2.IMREAD_COLOR)
+    #     if template is None:
+    #         raise ValueError(f"无法加载模板图像: {template_path}")
+    #     else :
+    #         print('cv2好使辣')
+    #     self.template_images[template_name] = template
+    #     print(f"已加载模板: {template_name}")
 
-        Args:
-            template_name (str): 模板名称
-            template_path (str): 模板图像文件路径
-        """
-        if not os.path.exists(template_path):
-            raise FileNotFoundError(f"模板文件不存在: {template_path}")
 
-        template = cv2.imread(template_path, cv2.IMREAD_COLOR)
-        if template is None:
-            raise ValueError(f"无法加载模板图像: {template_path}")
-        else :
-            print('cv2好使辣')
-        self.template_images[template_name] = template
-        print(f"已加载模板: {template_name}")
-
-
-
-    def find_pattern(self, template_name, region=None):
+    def find_pattern(self, region=None):
         """
         在屏幕上查找指定模板
 
         Args:
-            template_name (str): 要查找的模板名称
             region (tuple): 搜索区域 (x, y, width, height)，None表示全屏
 
         Returns:
-            dict: 包含位置和置信度的字典，未找到返回None
+            rotated_rect: 轮廓的最小外接矩形列表，未找到返回None
         """
-        if template_name not in self.template_images:
-            raise ValueError(f"未找到模板: {template_name}")
 
         # 截取屏幕
         screenshot = pyautogui.screenshot(region=region)
-        screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+        screenshot = preprocess(np.array(screenshot))
 
-        template = self.template_images[template_name]
+        # 查找符合条件的轮廓，并返回最小外接矩形
+        result = find_contours(screenshot, 100)
+        rotated_rect=[]
+        for cnt in result:
+            rotated_rect.append(cv2.minAreaRect(cnt))
 
-        # 模板匹配
-        result = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        return rotated_rect
 
-        if result is not None:
-            top_five = get_top_n_values(result)
-        else:
-            print("未找到匹配的图案或 target_info 为空")
-            return False
-        print(top_five)
-        if max_val >= self.confidence_threshold:
-            # 计算中心点坐标
-            h, w = template.shape[:2]
-            center_x = max_loc[0] + w // 2
-            center_y = max_loc[1] + h // 2
-
-            if region:
-                center_x += region[0]
-                center_y += region[1]
-
-            return {
-                'position': (center_x, center_y),
-                'top_left': (max_loc[0], max_loc[1]),
-                'bottom_right': (max_loc[0] + w, max_loc[1] + h),
-                'size': (w, h),
-                'confidence': max_val,
-                'target_info': result
-            }
-
-        return None
-
-    def wait_for_pattern(self, template_name, timeout=30000, interval=1, region=None):
-        """
-        等待图案出现
-
-        Args:
-            template_name (str): 要等待的模板名称
-            timeout (int): 超时时间（秒）
-            interval (float): 检查间隔（秒）
-            region (tuple): 搜索区域
-
-        Returns:
-            dict: 找到的图案信息，超时返回None
-        """
-        start_time = time.time()
-
-        while time.time() - start_time < timeout:
-            result = self.find_pattern(template_name, region)
-            if result:
-                return result
-
-
-        return None
-
-    def type_at_pattern(self, template_name, text, wait_timeout=30, region=None, offset_x=0, offset_y=0):
+    def type_at_pattern(self, text, region=None, offset_x=0, offset_y=0):
         """
         在图案位置输入文本，支持偏移
 
         Args:
-            template_name (str): 模板名称
             text (str): 要输入的文本
-            wait_timeout (int): 等待图案出现的超时时间
             region (tuple): 搜索区域
             offset_x (int): 相对于模板中心的X轴偏移量
             offset_y (int): 相对于模板中心的Y轴偏移量
@@ -211,19 +193,17 @@ class ScreenPatternAutomation:
         Returns:
             bool: 成功输入返回True，否则False
         """
-        pattern_info = self.wait_for_pattern(template_name, wait_timeout, region=region)
-        if pattern_info and 'target_info' in pattern_info and pattern_info['target_info'] is not None:
-            top_five = get_top_n_values(pattern_info['target_info'])
-        else:
-            print("未找到匹配的图案或 target_info 为空")
-            return False
-        for value , position in top_five:
-            x, y = position
-            # 应用偏移
-            target_x = x + offset_x
-            target_y = y + offset_y
-            pyautogui.write(text)
+        pattern_info = self.find_pattern(region=region)
+        if pattern_info is None:
 
+            return False
+        print('长度为',len(pattern_info))
+        for rect in pattern_info:
+            center_x, center_y = map(int, rect[0])            # 应用偏移
+            target_x = center_x + offset_x
+            target_y = center_y + offset_y
+
+            pyautogui.write(text)
             pyautogui.click(target_x, target_y)  # 先点击定位
         return True
 
@@ -232,14 +212,24 @@ class ScreenPatternAutomation:
 def main():
     # 创建自动化实例
     automation = ScreenPatternAutomation(confidence_threshold=0.99)
-
+    monitor = KeyboardMonitor()
+    monitor.start()
     try:
-        automation.load_template('water', 'needWater.png')
 
-        while monitor.is_alive():
-
-
-            automation.type_at_pattern('water', '1', offset_y=0, offset_x=10, wait_timeout=10)
+        while True :
+            status = monitor.status
+            tool = monitor.tool
+            if status == 'shutdown':
+                print('我结束辣')
+                break
+            elif status == 'start':
+                print('我开始辣')
+                automation.type_at_pattern(tool, offset_y=58, offset_x=-79)
+            elif status == 'pause':
+                print('我休眠辣')
+                while monitor.status == 'pause':
+                    time.sleep(1)
+        exit()
 
     except Exception as e:
         print(f"执行过程中出现错误: {e}")
